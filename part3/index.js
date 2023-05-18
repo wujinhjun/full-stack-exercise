94927e963543a4cde363649c884525ae316e8e92
wujinhjun <wujinhjun@sjtu.edu.cn>
2023-05-18 15:20:47 +0800

    feat: exercise 3.9-3.11

--- part3/index.js
+++ part3/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
 
@@ -10,6 +11,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
+app.use(cors());
+app.use(express.static("build"));
 
 const weeks = ["Mon", "Tues", "Wed", "Thurs", "Fri", "Sat", "Sun"];
 
@@ -142,7 +145,8 @@ app.delete("/api/persons/:id", (req, res) => {
   }
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
