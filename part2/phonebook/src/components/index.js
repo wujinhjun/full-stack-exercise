3f2879df665d3674b04b01dec83de4ccc889cdc6
wujinhjun <wujinhjun@sjtu.edu.cn>
2023-05-24 16:21:39 +0800

    refactor: update the structure of files

--- part2/phonebook/src/components/index.js
+++
@@ -1,6 +0,0 @@
-import Filter from "./Filter";
-import PersonForm from "./PersonForm";
-import Persons from "./Person";
-import Notification from "./Notification";
-
-export { Filter, PersonForm, Persons, Notification };
